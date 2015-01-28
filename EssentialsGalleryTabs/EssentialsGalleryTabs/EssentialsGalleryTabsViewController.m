088af5536f6c853c04921589d58713f91d789732
Daniel Allsop <DAllsop@scottlogic.com>
2015-01-28 13:00:22 +0000

    updated comments

--- EssentialsGalleryTabs/EssentialsGalleryTabs/EssentialsGalleryTabsViewController.m
+++ EssentialsGalleryTabs/EssentialsGalleryTabs/EssentialsGalleryTabsViewController.m
@@ -112,13 +112,12 @@
     [imageView setFrame:CGRectMake(leftHandPadding, 60 + padding, size.width, size.height)];
   
 
-    // The coordinates of the bezier path must be specified in container coordinates.
+    // We need to add an exclusion path to the text view so the text will wrap around the image.
+    // The coordinates of the exclusion path need to be specified in container coordinates like so
     CGRect imageFrame = [textView convertRect:imageView.bounds fromView:imageView];
     imageFrame.origin.x -= textView.textContainerInset.left;
     imageFrame.origin.y -= textView.textContainerInset.top;
-  
-    // An exclusionPaths can be set as a property on NSTextContainer to specify where
-    // UIBezierPaths that should not be filled with text.
+
     UIBezierPath *exclusionPath;
     if (leftPosition) {
       exclusionPath = [UIBezierPath bezierPathWithRect:CGRectMake(CGRectGetMinX(imageFrame),
